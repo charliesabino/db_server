3d0352b03740ba8f26d996ba9a8b42150b4c60da
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-06-24 10:34:27 -0400

    Create basic server

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Any
+import socket
 
 class Database:
     def __init__(self):
@@ -10,9 +11,39 @@ class Database:
     def set(self, key: str, value: Any) -> None:
         self.kv_store[key] = value
 
+class Server:
+    def __init__(self):
+        self.db = Database()
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(("127.0.0.1", 4000))
+        self.server.listen()
+        self.buffer = b""
+    
+    def accept(self):
+        return self.server.accept()
+    
+    def handle_client(self, client_socket: socket.socket):
+        while True:
+            self.handle_request(client_socket)
+    
+    def handle_request(self, client_socket: socket.socket):
+        self.buffer += client_socket.recv(1024)
+        if not self.buffer:
+            return
+        print(self.buffer)
+        self.buffer = b""
+
+    def close(self):
+        self.server.close()
 
 def main():
-    db = Database()
+    server = Server()
+
+    # could employ threads / asyncio / etc. here to handle multiple clients; omitted for simplicity
+    while True:
+        client_socket, client_address = server.accept()
+        server.handle_client(client_socket)
+        client_socket.close()
 
 if __name__ == "__main__":
     main()
