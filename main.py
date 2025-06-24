62baba8060cdb25822fb7caf7ae81e90b8f466b6
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-06-24 11:34:21 -0400

    Fix typing

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from typing import Any
+from typing import Optional
 import socket
 
 class Database:
     def __init__(self):
         self.kv_store = {}
 
-    def get(self, key: str) -> str:
+    def get(self, key: str) -> Optional[str]:
         return self.kv_store.get(key)
 
     def set(self, key: str, value: str) -> None:
