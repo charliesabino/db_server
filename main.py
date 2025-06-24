33d1576a8be6cb347e1c4e7cb8f5b87b77f06d62
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-06-24 10:26:30 -0400

    Create DB class

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from db-server!")
+from typing import Any
 
+class Database:
+    def __init__(self):
+        self.kv_store = {}
+    
+    def get(self, key: str) -> Any:
+        return self.kv_store.get(key)
+    
+    def set(self, key: str, value: Any) -> None:
+        self.kv_store[key] = value
+
+
+def main():
+    db = Database()
 
 if __name__ == "__main__":
     main()
